804447fe4d4838ffa696add0ef11f3bd80e64a89
Cristian Borodin <cristiborodin@gmail.com>
2022-01-13 14:22:21 +0200

    Create unit.test.js

---
+++ unit.test.js
@@ -0,0 +1,59 @@
+const request = require("supertest");
+const {
+	app,
+	server,
+	db,
+	dbConnection,
+	getDbMessages
+} = require("./server.js");
+
+
+describe('jest-junit', () => {
+	describe('positive numbers', () => {
+		it('should add up', () => {
+			expect(1 + 2).toBe(3);
+		});
+	});
+});
+
+test("GET / method statusCode should be 200", done => {
+	request(app)
+		.get("/")
+		.then(response => {
+			expect(response.statusCode).toBe(200);
+			done();
+		});
+});
+
+test("GET /database statusCode should be 200", done => {
+	request(app)
+		.get("/database")
+		.then(response => {
+			expect(response.statusCode).toBe(200);
+			done();
+		});
+});
+
+test('Check if database is connected', done => {
+	function callback(data) {
+		expect(data).toBe(true);
+		done();
+	}
+
+	dbConnection(callback);
+});
+
+
+test('Max 100 chat messages', done => {
+	function callback(data) {
+		expect(data).toBeLessThan(100);
+		done();
+	}
+
+	getDbMessages(callback);
+});
+
+afterAll(() => {
+	db.end();
+	server.close();
+});
